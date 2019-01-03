21e8b85776d8b0ae18c44f38b11f4ec62e289698
tuqiang01 <tuqiang01@baidu.com>
2019-01-03 11:33:26 +0800

    修正webpack

--- src/app.js
+++ src/app.js
@@ -1,33 +1,93 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {StaticMap} from 'react-map-gl';
-import DeckGL, {LineLayer, ScatterplotLayer} from 'deck.gl';
+import DeckGL, {LineLayer, ScatterplotLayer, PolygonLayer} from 'deck.gl';
+import {TripsLayer} from "@deck.gl/experimental-layers"
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 
-const INITIAL_VIEW_STATE = {
-  latitude: 37.785164,
-  longitude: -122.41669,
-  zoom: 16,
-  bearing: -20,
-  pitch: 60
+// Source data CSV
+const DATA_URL = {
+    BUILDINGS:
+        'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/buildings.json', // eslint-disable-line
+    TRIPS:
+        'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/trips.json' // eslint-disable-line
 };
 
-console.log(MAPBOX_TOKEN);
+const INITIAL_VIEW_STATE = {
+    longitude: -74,
+    latitude: 40.72,
+    zoom: 13,
+    maxZoom: 16,
+    pitch: 45,
+    bearing: 0
+};
+const LIGHT_SETTINGS = {
+    lightsPosition: [-74.05, 40.7, 8000, -73.5, 41, 5000],
+    ambientRatio: 0.05,
+    diffuseRatio: 0.6,
+    specularRatio: 0.8,
+    lightsStrength: [2.0, 0.0, 0.0, 0.0],
+    numberOfLights: 2
+};
+
 class Root extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: 0
+        };
+    }
+
+    componentDidMount() {
+        this._animate();
+    }
+    componentWillUnmount() {
+        if (this._animationFrame) {
+            window.cancelAnimationFrame(this._animationFrame);
+        }
+    }
+    _animate() {
+        const {
+            loopLength = 1800, // unit corresponds to the timestamp in source data
+            animationSpeed = 30 // unit time per second
+        } = this.props;
+        const timestamp = Date.now() / 1000;
+        const loopTime = loopLength / animationSpeed;
+
+        this.setState({
+            time: ((timestamp % loopTime) / loopTime) * loopLength
+        });
+        this._animationFrame = window.requestAnimationFrame(this._animate.bind(this));
+    }
   render() {
     return (
-      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} width="100%" height="100%">
-        <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} />
-        <LineLayer
-          data={[{sourcePosition: [-122.41669, 37.7883], targetPosition: [-122.41669, 37.781]}]}
-          strokeWidth={5}
-        />
-        <ScatterplotLayer
-          data={[{position: [-122.41669, 37.79]}]}
-          radiusScale={100}
-          getColor={x => [0, 0, 255]}
+      <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          width="100%"
+          height="100%">
+          <PolygonLayer
+              id = "buildings"
+              data = {DATA_URL.BUILDINGS}
+              extruded = {true}
+              wireframe = {false}
+              fp64 = {true}
+              getFillColor ={[74,80,87]}
+              lightSettings = {LIGHT_SETTINGS}
+              opacity = {0.5}
+              getPolygon = {f => f.polygon}
+              getElevation = { f => f.height}
+          />
+          <TripsLayer
+              id = {'id'}
+              data = {DATA_URL.TRIPS}
+          />
+        <StaticMap
+            reuseMaps
+            mapStyle="mapbox://styles/mapbox/dark-v9"
+            mapboxApiAccessToken={MAPBOX_TOKEN}
         />
       </DeckGL>
     );
